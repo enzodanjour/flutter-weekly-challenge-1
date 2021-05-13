87dbb8c8f241e15a43eb252a2a7e7c3ca6ca6b4f
enzo <enzo.danjour@gmail.com>
2021-05-13 10:45:40 -0300

    Initial layout of LoginPage

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutterweeklychallenge1/core/app_gradients.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: AppGradients.linearHorizontal
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [
+            RichText(
+              text: TextSpan(
+                text: 'Welcome to \n',
+                style: TextStyle(
+                  fontFamily: 'Poppins'
+                )
+              )
+            ),
+            Center(
+              child: Icon(
+                Icons.store_mall_directory_rounded,
+                color: Colors.white,
+                size: 150,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
