0494d5783383eb23dd742a57d39cd09d6d1308a2
enzo <enzo.danjour@gmail.com>
2021-05-13 11:25:43 -0300

    Initial contants of collors

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final light = Colors.grey[50];
+  static final lightDetail = Colors.grey[100];
+  static final purple = Colors.purple[300];
+  static final pinklight = Colors.pink[300];
+  static final pinkDark = Colors.pink[500];
+  static final pink = Colors.pink[400];
+}
