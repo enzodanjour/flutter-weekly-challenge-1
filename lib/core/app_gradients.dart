cb9b325a9e661bc1a742fb16d53d8adb13963259
enzo <enzo.danjour@gmail.com>
2021-05-13 10:44:16 -0300

    Create constant to gradient

---
+++ lib/core/app_gradients.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppGradients {
+  static final linearHorizontal = LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [Colors.pink[500], Colors.pink[300]]);
+}
